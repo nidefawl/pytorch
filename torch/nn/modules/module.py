8743a25254b1f7a7522c9289e859f0e260a0c9cc
Mikayla Gawarecki <mikaylagawarecki@gmail.com>
2024-01-10 16:10:11 -0800

    Add swap_tensors path to nn.Module._apply
    ghstack-source-id: 7c5bfa9cc07c847e00765ac843347f4ba392e7fa
    Pull Request resolved: https://github.com/pytorch/pytorch/pull/117167

--- torch/nn/modules/module.py
+++ torch/nn/modules/module.py
@@ -13,6 +13,7 @@ from torch import Tensor, device, dtype
 from typing import Union, Tuple, Any, Callable, Iterator, Set, Optional, overload, TypeVar, Mapping, Dict, List
 from typing_extensions import Self
 from ...utils.hooks import RemovableHandle
+from weakref import getweakrefcount
 
 __all__ = ['register_module_forward_pre_hook', 'register_module_forward_hook',
            'register_module_full_backward_pre_hook', 'register_module_backward_hook',
@@ -815,6 +816,14 @@ class Module:
             else:
                 return False
 
+        def compute_should_use_swap_tensors(tensor):
+            if torch.__future__.get_swap_module_params_on_conversion():
+                swap_param = torch._C._tensor_use_count(tensor._cdata) == 1 and getweakrefcount(tensor) == 0
+                swap_grad = torch._C._tensor_use_count(tensor.grad._cdata) == 1 and getweakrefcount(tensor.grad) == 0
+                return (swap_param, swap_grad)
+            else:
+                return (False, False)
+
         for key, param in self._parameters.items():
             if param is None:
                 continue
@@ -823,26 +832,46 @@ class Module:
             # `with torch.no_grad():`
             with torch.no_grad():
                 param_applied = fn(param)
-            should_use_set_data = compute_should_use_set_data(param, param_applied)
-            if should_use_set_data:
-                param.data = param_applied
-                out_param = param
+            p_should_use_set_data = compute_should_use_set_data(param, param_applied)
+            p_should_use_swap_tensors, g_should_use_swap_tensors = compute_should_use_swap_tensors(param)
+            param_grad = param.grad
+            if g_should_use_swap_tensors:
+                # grad use_count was 1 according to g_should_use_swap_tensors but
+                # was bumped by param_grad = param.grad, decrement it so swapping grad works
+                param.grad = None
+            if p_should_use_set_data:
+                if p_should_use_swap_tensors:
+                    param_applied = torch.nn.Parameter(param_applied, requires_grad=param.requires_grad)
+                    torch.utils.swap_tensors(param, param_applied)
+                    out_param = param
+                else:
+                    param.data = param_applied
+                    out_param = param
             else:
                 assert isinstance(param, Parameter)
                 assert param.is_leaf
                 out_param = Parameter(param_applied, param.requires_grad)
                 self._parameters[key] = out_param
 
-            if param.grad is not None:
+            if param_grad is not None:
                 with torch.no_grad():
-                    grad_applied = fn(param.grad)
-                should_use_set_data = compute_should_use_set_data(param.grad, grad_applied)
-                if should_use_set_data:
-                    assert out_param.grad is not None
-                    out_param.grad.data = grad_applied
+                    grad_applied = fn(param_grad)
+                g_should_use_set_data = compute_should_use_set_data(param_grad, grad_applied)
+                if g_should_use_set_data:
+                    if g_should_use_swap_tensors:
+                        grad_applied.requires_grad_(param_grad.requires_grad)
+                        torch.utils.swap_tensors(param_grad, grad_applied)
+                        out_param.grad = param_grad
+                    else:
+                        if p_should_use_swap_tensors:
+                            param_grad.data = grad_applied
+                            out_param.grad = grad_applied
+                        else:
+                            assert out_param.grad is not None
+                            out_param.grad.data = grad_applied
                 else:
-                    assert param.grad.is_leaf
-                    out_param.grad = grad_applied.requires_grad_(param.grad.requires_grad)
+                    assert param_grad.is_leaf
+                    out_param.grad = grad_applied.requires_grad_(param_grad.requires_grad)
 
         for key, buf in self._buffers.items():
             if buf is not None:
